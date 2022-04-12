68813c55050959f7a72f1b12b15e87d3aae3e5c5
Matt E <matteweva@gmail.com>
2022-04-12 08:50:33 -0700

    removing useState from Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import "./Home.css"
 import Header from '../../components/Header/Header'
 import Sidebar from "../../components/Sidebar/Sidebar"

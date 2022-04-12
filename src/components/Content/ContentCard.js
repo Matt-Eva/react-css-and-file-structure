913339605bd5595d69f62bd8b57154218b2a47e3
Matt E <matteweva@gmail.com>
2022-04-12 09:02:52 -0700

    restructured props in content card

--- src/components/Content/ContentCard.js
+++ src/components/Content/ContentCard.js
@@ -2,12 +2,12 @@ import React from 'react'
 import "./ContentCard.css"
 import Button from "react-bootstrap/Button"
 
-function ContentCard(props) {
+function ContentCard({isBlue}) {
 
-  const isBlue = props.isBlue ? "content-card--blue" : "";
+  const blueBackground = isBlue ? "content-card--blue" : "";
   
   return (
-    <div className={`content-card ${isBlue}`}>
+    <div className={`content-card ${blueBackground}`}>
       <h2 className="content-card__header"> Header</h2>
       <h2 className="content-card__header content-card__header--green">Green Header</h2>
       <div className="content-card__button-container">

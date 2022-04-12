76b69554010e4a501c5a6906bd0e1ead93179da4
Matt E <matteweva@gmail.com>
2022-04-12 08:41:33 -0700

    classname consistency in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,8 +8,8 @@ function Header(/*{toggleSidebar, toggleModal}*/) {
     <div className="header">
         <ToggleSidebar /*toggleSidebar={toggleSidebar}*/ />
         <h1 className="header__title">Logo</h1>
-        <Link to="/" className="header__home-link">Home</Link>
-        <Link to="/about" className="header__about-link">About</Link>
+        <Link to="/" className="header__link header__home-link">Home</Link>
+        <Link to="/about" className="header__link header__about-link">About</Link>
         <button className="header__toggle-modal-btn" /*onClick={toggleModal}*/>Toggle Modal</button>
     </div>
   )
